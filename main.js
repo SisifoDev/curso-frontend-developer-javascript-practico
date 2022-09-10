ee720bf9a5e3f68effa3e08369ef29726627be16
sisifodev <manuelperdomo1710@icloud.com>
2022-09-10 09:56:33 -0500

    feat: Add mobile menu

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 
+const menuHamburger = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+
 menuEmail.addEventListener("click", toogleDesktopMenu);
+menuHamburger.addEventListener("click", toogleMobileMenu);
 
 function toogleDesktopMenu() {
   desktopMenu.classList.toggle("inactive");
+}
+
+function toogleMobileMenu() {
+  mobileMenu.classList.toggle("inactive");
 }
